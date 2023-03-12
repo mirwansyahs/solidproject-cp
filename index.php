bb2aad5ad34de94a4df3498b4d79791a6d9fc54d
Mohammad Irwansyah Somantri <mirwansyahs@MacBook-Pro-mohammad.local>
2023-03-13 03:17:37 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+  <head>
+
+    <!-- Display -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  
+    <!-- Identity -->
+    <title>SOLID PROJECT</title>
+    <meta name="description" content="Solid Project is a creative multipurpose theme for building websites. It is based on Bootstrap 5 and other of the best and most up-to-date code libraries used by professional developers in the world.">
+    <meta name="author" content="Solid Project Team">
+    <link rel="shortcut icon" href="assets/images/favicon-spt.ico" type="image/x-icon" />
+  
+    <!-- Vendor Style Sheet -->
+    <link rel="stylesheet" href="assets/css/vendor/preloader.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/font-family.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/font-awesome.min.css" />  
+    <link rel="stylesheet" href="assets/css/vendor/menu-engine.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/menu-grid.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/swiper.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/dynamic-slider.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/bricklayer.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/lightbox.min.css" />
+    <link rel="stylesheet" href="assets/css/vendor/aos.min.css" />
+  
+    <!-- Main Style Sheet -->
+    <link rel="stylesheet" href="assets/css/theme.css" />
+    <link rel="stylesheet" href="assets/css/core.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+  
+  </head>
+
+  <body class="shock-body">
+
+    <!-- Preloader -->
+    <div id="preloader" class="preloader white" data-delay="0" data-limit="3000">
+      <img src="assets/images/logo-spt.svg" class="emblem" alt="Emblem" />
+    </div>
+
+    <!-- Header -->
+    <header id="header" class="shock-header">
+      <!-- Navbar -->
+      <nav id="navbar" class="navbar navbar-centered navbar-expand-lg auto-hide scheme-3 tertiary">
+        <div class="container-fluid">
+          <!-- Brand -->
+          <a class="navbar-brand magnetic-effect" href="/">
+            <!-- <div class="site-title">Solid Project</div> -->
+            <img src="assets/images/logo-spt.svg" data-logo-alt="assets/images/logo-spt.svg" data-logo-mobile="assets/images/logo-spt.svg" alt="Solid Project" width="50px" class="logo" />
+          </a>
+          <!-- Responsive menu toggle -->
+          <button class="navbar-toggler" data-bs-target="#navbar-items" data-bs-toggle="collapse" aria-expanded="false">
+            <span class="navbar-toggler-icon">
+              <span class="line"></span>
+              <span class="line"></span>
+              <span class="line"></span>
+            </span>
+          </button>
+          <div id="navbar-items" class="collapse navbar-collapse">
+            <!-- Responsive search form -->
+            <div class="navbar-left">
+              <!-- Link -->
+              <ul class="navbar-nav">
+                
+                <li class="nav-item">
+                  <a href="./" target="_blank" class="nav-link flutter-underline">
+                    <span class="text">Home</span>
+                    <svg class="flutter-underline-graphic" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+                      <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"></path>
+                    </svg>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#portfolio" target="_blank" class="nav-link flutter-underline">
+                    <span class="text">Portofolio</span>
+                    <svg class="flutter-underline-graphic" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+                      <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"></path>
+                    </svg>
+                  </a>
+                </li>
+                <!-- <li class="nav-item">
+                  <a href="#" target="_blank" class="nav-link flutter-underline">
+                    <span class="text">About us</span>
+                    <svg class="flutter-underline-graphic" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+                      <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"></path>
+                    </svg>
+                  </a>
+                </li> -->
+                
+              </ul>
+            </div>
+            <div class="navbar-right">
+              <!-- Link -->
+              <ul class="navbar-nav">
+                
+                
+                <!-- <li class="nav-item">
+                  <a href="#" target="_blank" class="nav-link flutter-underline">
+                    <span class="text">Career</span>
+                    <svg class="flutter-underline-graphic" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+                      <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"></path>
+                    </svg>
+                  </a>
+                </li> -->
+                <li class="nav-item">
+                  <a href="#contact" target="_blank" class="nav-link flutter-underline">
+                    <span class="text">Contact us</span>
+                    <svg class="flutter-underline-graphic" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+                      <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"></path>
+                    </svg>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" target="_blank" class="nav-link flutter-underline">
+                    <span class="text">Login / Register</span>
+                    <svg class="flutter-underline-graphic" width="300%" height="100%" viewBox="0 0 1200 60" preserveAspectRatio="none">
+                      <path d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"></path>
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Main -->
+    <main id="main" class="shock-main" data-bg-color="#141415">
+      
+      <!-- Banner -->
+      <section class="shock-section bg-image bg-fixed" data-bg-image="assets/images/b1.png">
+        <div class="container">
+          <div class="holder vh-100 align-v-center">
+            <!-- Intro -->
+            <div class="extended-intro">
+              <div class="wrapper">
+                <div class="left-column">
+                  <h2 class="title text-style-1 text-offset">
+                    <span class="text-1 filled tertiary-50">Solid Project</span>
+                    <span class="text-1 outline white" data-lax="inertia-top">Solid Project</span>
+                  </h2>
+                  <span class="text-2 text-style-2 fw-400 text-outline text-italic gray">turning ideas into results.</span>
+                  <div class="description text-style-12 gray">
+                    <p>We keep in mind that we always need to improve everything we do, aiming to help people with our products and services, prioritizing quality.</p>
+                  </div>                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Banner -->
+      <section class="shock-section bg-image bg-fixed" data-bg-image="assets/images/b2.png">
+        <div class="holder vh-100"></div>
+      </section>
+
+      <!-- Carousel -->
+      <section class="shock-section pt-6 pb-4" id="portfolio">
+        <div class="container max-w-85">
+          <div class="row g-4">
+            <div class="col-12 col-md-6 align-v-center">
+              <!-- Intro -->
+              <div class="side-intro">
+                <h1 class="title text-style-1 text-offset">
+                  <span class="text-1 filled tertiary-50">We build</span>
+                  <span class="text-1 outline white" data-lax="inertia-top">We build</span>
+                </h1>
+                <span class="text-2 text-style-4 fw-400 text-outline text-italic gray">amazing projects.</span>
+                <div class="description text-style-12 gray">
+                  <p>We use the best technologies available on the market to ensure that our customers obtain positive results.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <!-- Carousel -->
+              <div class="gallery swiper slider has-navigation has-scrollbar scheme-2 tertiary" data-columns="1,1,1,1" data-autoplay="10000" data-loop="true">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <!-- Video -->
+                    <a href="https://youtu.be/Nm4PsWAS66I" class="item active lightbox-link hover-zoom">
+                      <i class="fa-solid fa-circle-play gallery-icon floating-item white"></i>
+                      <div class="image-wrapper">
+                        <img src="assets/images/qrmoney.png" class="image" alt="Sistem Parkir menggunakan QR Code sudah mulai digunakan oleh banyak plaza atau pusat perbelanjaan, yang diintegrasikan dengan sistem pembayaran QRIS Gojek, Dana dan e-Wallet lainnya. Ini merupakan sistem yang hanya digunakan untuk pribadi atau skala perusahaan sendiri." />
+                      </div>
+                    </a>
+                  </div>
+                  <div class="swiper-slide">
+                    <a href="https://youtu.be/2yQJ34DSAxI" class="item lightbox-link hover-zoom">
+                      <i class="fa-solid fa-circle-play gallery-icon floating-item white"></i>
+                      <div class="image-wrapper">
+                        <img src="assets/images/pembukuan.png" class="image" alt="Sistem Informasi Pembukuan SMK Presiden Kota Cirebon merupakan sebuah aplikasi yang akan mempermudah sekolah dalam mengelola pembayaran keuangan siswa, juga terdapat fitur untuk mengelola jurnal, sehingga pekerjaan yang dilakukan akan lebih cepat dan efisien." />
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div class="swiper-button-prev "></div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-scrollbar"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- Divider -->
+      <section class="shock-section pt-4 pb-4">
+        <div class="container max-w-85">
+          <span class="zzz scheme-2 tertiary-50"></span>
+        </div>
+      </section>
+
+      
+      <!-- Form -->
+      <section class="shock-section has-overlay pt-5 pb-5 bg-image bg-fixed" data-bg-image="assets/images/laptopontable.webp" id="contact">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-6 align-v-center">       
+              <!-- Intro -->
+              <div class="side-intro mb-35">
+                <h2 class="title text-style-1 text-offset">
+                  <span class="text-1 filled tertiary-50">Service</span>
+                  <span class="text-1 outline white" data-lax="inertia-top">Service</span>
+                </h2>
+                <span class="text-2 text-style-4 fw-400 text-outline text-italic gray">custom made.</span>
+                <div class="description text-style-12 gray">
+                  <p>To connect a brand with a target audience, it is necessary to pay attention to every flow, shape, color, font and style that will compose a subconscious message.</p>
+                </div>
+                
+                <!-- Button -->
+                <a href="#" class="button double-edge transparent tertiary-hover">
+                  <span class="button-text white white-hover">Join with us</span>
+                  <i class="fa-solid fa-arrow-right button-icon white white-hover"></i>
+                  <span class="overlay gray-25 magnetic-effect"></span>
+                </a>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <!-- Overlay -->
+        <div class="overlay black-25"></div>
+      </section>
+
+    </main>
+
+    <!-- Vertical Lines -->
+    <div class="vertical-lines diagonal scheme-2 tertiary">
+      <div class="row">
+        <div class="col-12 col-md-6 align-h-center">
+          <span class="line"></span>
+        </div>
+        <div class="col-12 col-md-6 align-h-center">
+          <span class="line"></span>
+        </div>
+      </div>
+    </div>
+
+    <!-- Side Widget -->
+    <div class="side-widget to-left invert-color mix-blend-difference d-only-desktop">
+      <div class="item">
+        <span class="widget label-icons">
+          <a href="https://instagram.com/solidproject.id" class="link black black-hover"><i class="icon fab fa-instagram"></i></a>
+          <span class="label-line black"></span>
+        </span>
+      </div>
+    </div>
+
+    <!-- Scroll to Top -->
+    <div class="side-widget to-right invert-color mix-blend-difference">
+      <div class="item align-v-bottom">
+        <a href="#" class="link hover-up">
+          <span class="widget float-icon">
+            <i class="fa-solid fa-arrow-up-long icon"></i>
+          </span>
+        </a>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <footer id="footer" class="shock-footer scheme-1 tertiary" data-bg-color="#1a1a1b">
+      <div class="bottom-bar" data-bg-color="#1a1a1b">
+        <div class="text">© 2020 - <?=date('Y')?> - All rights reserved. The <a href="https://solidproject.id/" class="link gray primary-hover"><u>Solid Project</u></a> is developed and maintained by <a href="https://solidproject.id/" class="link gray primary-hover"><u>Solid Project Team</u></a>.</div>
+      </div>
+    </footer>
+    
+    <!-- Cursor -->
+    <svg class="cursor-effect tertiary" width="220" height="220" viewBox="0 0 220 220">
+      <defs>
+        <filter id="cursor-effect-filter" x="-50%" y="-50%" width="200%" height="200%" filterUnits="objectBoundingBox">
+          <feTurbulence type="fractalNoise" baseFrequency="0" numOctaves="1" result="warp"></feTurbulence>
+          <feOffset dx="-30" result="warpOffset"></feOffset>
+          <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="20" in="SourceGraphic" in2="warpOffset"></feDisplacementMap>
+        </filter>
+      </defs>
+      <circle class="cursor-effect-inner" cx="110" cy="110" r="20"></circle>
+    </svg>
+
+    <!-- Vendor JavaScript -->
+    <script src="assets/js/vendor/jquery.min.js"></script>
+    <script src="assets/js/vendor/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/vendor/preloader.min.js"></script>
+    <script src="assets/js/vendor/inview.min.js"></script>
+    <script src="assets/js/vendor/menu-engine.min.js"></script>
+    <script src="assets/js/vendor/menu-grid.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <script src="assets/js/vendor/swiper.min.js"></script>
+    <script src="assets/js/vendor/anime.min.js"></script>
+    <script src="assets/js/vendor/dynamic-slider.min.js"></script>
+    <script src="assets/js/vendor/shuffle.min.js"></script>
+    <script src="assets/js/vendor/stickybits.min.js"></script>
+    <script src="assets/js/vendor/bricklayer.min.js"></script>
+    <script src="assets/js/vendor/lightbox.min.js"></script>
+    <script src="assets/js/vendor/typed.min.js"></script>
+    <script src="assets/js/vendor/progressbar.min.js"></script>
+    <script src="assets/js/vendor/map-styles.min.js"></script>
+    <script src="assets/js/vendor/magnetic-effect.min.js"></script>
+    <script src="assets/js/vendor/gsap.min.js"></script>
+    <script src="assets/js/vendor/aos.min.js"></script>
+    <script src="assets/js/vendor/lax.min.js"></script>
+    <script src="assets/js/vendor/cursor-effect.min.js"></script>
+
+    <!-- Main JavaScript -->
+    <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
